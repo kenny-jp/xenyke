8854d92a5a4d3735dec59698f430735f5be90af2
kenny-jp <kenny.jacquesphilippe@gmail.com>
2024-05-04 15:49:23 +0200

    global include

---
+++ include/xenyke/core.hpp
@@ -0,0 +1,3 @@
+# include <xenyke/core/color.hpp>
+# include <xenyke/core/rect.hpp>
+# include <xenyke/core/vec.hpp>
---
+++ include/xenyke/gl.hpp
@@ -0,0 +1,6 @@
+# include <xenyke/gl/init.hpp>
+# include <xenyke/gl/monitor.hpp>
+# include <xenyke/gl/shader_program.hpp>
+# include <xenyke/gl/texture.hpp>
+# include <xenyke/gl/vertex_buffer.hpp>
+# include <xenyke/gl/window.hpp>
