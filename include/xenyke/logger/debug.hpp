1c499678356ce2c84e11dfe4e76cf86c10445826
kenny-jp <kenny.jacquesphilippe@gmail.com>
2024-05-09 18:50:39 +0200

    remove macro xkeDebug

--- include/xenyke/logger/debug.hpp
+++ include/xenyke/logger/debug.hpp
@@ -3,10 +3,9 @@
 
 # include <xenyke/logger/console/console.hpp>
 
-# ifdef XKE_DEBUG
-#  define xkeDebug(...) xke::logger::console::console_default_t::debug(__VA_ARGS__)
-# else
-#  define xkeDebug(...)
-# endif
+XKE_NAMESPACE_BEGIN
+
+
+XKE_NAMESPACE_END
 
 # endif //XKE_LOGGER_LOGGER_HPP
