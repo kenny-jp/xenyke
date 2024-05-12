0ac948d5b2e8c5f68c231bfb67210eb44b8e76a2
kenny-jp <kenny.jacquesphilippe@gmail.com>
2024-05-13 01:33:22 +0200

    tests...

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,31 +1,47 @@
-#include <xenyke/core/color.hpp>
-#include <xenyke/core/debug.hpp>
+#include <xenyke/gl/init.hpp>
+#include <xenyke/gl/shader_program.hpp>
+#include <xenyke/gl/texture.hpp>
+#include <xenyke/gl/vertex_buffer.hpp>
+#include <xenyke/gl/window.hpp>
+
+#include <xenyke/gl/ext/glm/gtc/matrix_transform.hpp>
 
 int main()
 {
     using namespace xke;
 
-    xkeDebug() << "Size of ColorRgb: " << sizeof(ColorRgb) << '\n'
-               << "Size of ColorRgba: " << sizeof(ColorRgba) << xke::flush;
+    xkeinit();
 
-    {
-        ColorRgb r(ColorRgb::Red);
-        ColorRgb g(ColorRgb::Green);
-        ColorRgb b(ColorRgb::Blue);
-        xkeDebug() << r << '\n';
-        xkeDebug() << g << '\n';
-        xkeDebug() << b << '\n';
+    Window window(800, 800, "MyWin");
 
+    ShaderProgram program("../../../data/shaders/shader.glsl");
+
+
+    xke::EVertexBuffer<xke::BufferUsage::STATIC, xke::Primitive::LINES, 6> buff;
+    xke::VertexArray vertices(4, xke::Primitive::TRIANGLES);
+    vertices.setVertex(0, xke::Vertex(xke::Vec3d(-0.5, -0.5,  0.0), xke::Color::Red, xke::Vec2d(0, 0)));
+    vertices.setVertex(1, xke::Vertex(xke::Vec3d(-0.5,  0.5,  0.0), xke::Color::Red, xke::Vec2d(0, 1)));
+    vertices.setVertex(2, xke::Vertex(xke::Vec3d( 0.5,  0.5, 0.0),  xke::Color::Red, xke::Vec2d(1, 1)));
+    vertices.setVertex(3, xke::Vertex(xke::Vec3d( 0.5, -0.5, 0.0),  xke::Color::Red, xke::Vec2d(1, 0)));
+
+    buff.setIndexes({0, 1, 2, 0, 2, 3});
+    buff.writeIndexes();
+    buff.write(vertices);
+
+    program.use();
+    program.setBool("hasTexture", false);
+    program.setMat4("mvp", glm::mat4(1.f));
+
+
+    while(window.isOpen()) {
+        window.clear(Color::Black);
+
+        buff.draw();
+
+        window.display();
     }
 
-    {
-        ColorRgba r(ColorRgba::Red);
-        ColorRgba g(ColorRgba::Green);
-        ColorRgba b(ColorRgba::Blue);
-        xkeDebug() << r << '\n';
-        xkeDebug() << g << '\n';
-        xkeDebug() << b << '\n';
-    }
 
-    return 0;
+
+    return xkeTerminate();
 }
